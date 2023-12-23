ea6a3f017c856b437f794dabd80411150e0a46f2
Osama Rauf <osamarauf99@gmail.com>
2023-12-23 15:46:37 +0500

    Added Dockerfile for Frontend

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import psycopg2
+from flask import Flask, render_template, request, jsonify
 import requests
-import json
 
 app = Flask(__name__)
 
