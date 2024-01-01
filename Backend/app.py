aa9ae1c98fc17a5b0fe8b7ff9e46488da1e88b64
Osama Rauf <osamarauf99@gmail.com>
2024-01-01 20:18:39 +0500

    Changes in Backend app.py

--- Backend/app.py
+++ Backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 def create_db():
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}" # f"postgresql://postgres:root@postgres:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.getenv('DB_USER')}:{os.environ.getenv('DB_PASSWORD')}@{os.environ.getenv('DB_HOST')}:{os.environ.getenv('DB_PORT')}/{os.environ.getenv('DB_NAME')}" # f"postgresql://postgres:root@postgres:5432/postgres"
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return SQLAlchemy(app)
